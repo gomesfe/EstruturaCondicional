758fd14ac9dd5f2b99a49b27faa5ec115f7fc633
Felipe Gomes <felipelimagomes0@gmail.com>
2025-05-11 17:27:28 -0300

    first commit

---
+++ EstruturaDeSelecao-main/src/CalculaFuncao.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CalculaFuncao {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double y,x;
+
+        System.out.println("Digite o valor de X");
+        x = sc.nextDouble();
+        if(x > 5 || x< -5){
+            y = 8 / Math.sqrt(x * x - 25);
+            System.out.println("Y = " +y);
+        }
+        else {
+            System.out.println("Valor inválido para X");
+        }
+
+
+    }
+}
